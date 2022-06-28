1ee961d2ffbc31948c2b04cb0abed174327a9625
Matheus Ribeiro <matheus.junio159@gmail.com>
2022-06-27 21:02:58 -0300

    correção de erros git

--- src/components/rastreio/SituacaoRastreio.tsx
+++ src/components/rastreio/SituacaoRastreio.tsx
@@ -21,10 +21,6 @@ export default function SituacaoRastreio(props: SituacaoRastreioProps) {
   function pegarSituacao() {
     if (situacao === null) {
       api.get(`/situacao/${codigo}`).then((resposta) => {
-<<<<<<< HEAD
-=======
-        console.log(resposta.data);
->>>>>>> da4f1068b3dfbb833818bbf8a5266cb04728dd7b
         setSituacao(resposta.data);
       });
     }
--- src/components/template/Header.tsx
+++ src/components/template/Header.tsx
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-/* eslint-disable @next/next/no-sync-scripts */
-=======
->>>>>>> da4f1068b3dfbb833818bbf8a5266cb04728dd7b
 import Head from "next/head";
 import { useEffect } from "react";
 
@@ -19,19 +15,6 @@ export default function Header(props: HeaderProps) {
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <meta charSet="utf-8" />
       <link rel="icon" type="image/x-icon" href="/favicon.ico" />
-<<<<<<< HEAD
-      <link
-        rel="stylesheet"
-        type="text/css"
-        href="https://cdn.datatables.net/v/dt/dt-1.12.1/datatables.min.css"
-      />
-
-      <script
-        type="text/javascript"
-        src="https://cdn.datatables.net/v/dt/dt-1.12.1/datatables.min.js"
-      ></script>
-=======
->>>>>>> da4f1068b3dfbb833818bbf8a5266cb04728dd7b
     </Head>
   );
 }
